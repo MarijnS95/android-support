dd197c53719f8a35de42db086b5f51592213011b
Marijn Suijten <marijn@traverseresearch.nl>
2024-07-19 14:05:52 +0200

    Set `ContextClassLoader` on `currentThread()`

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use android_activity::AndroidApp;
 use jni::{
-    objects::{JClass, JObject, JValue},
+    objects::{JClass, JObject, JString, JValue},
     JavaVM,
 };
 
 pub fn enable_immersive(app: &AndroidApp) {
+    // std::env::set_var("RUST_BACKTRACE", "1");
+
     let vm = unsafe { JavaVM::from_raw(app.vm_as_ptr().cast()) }.unwrap();
 
     let mut env = vm.attach_current_thread().unwrap();
@@ -24,30 +26,73 @@ pub fn enable_immersive(app: &AndroidApp) {
         .unwrap();
     dbg!(&cl);
 
-    // Find our support class - it happens to be an Activity for now, but we just need one static
-    // method from it
-    let class_name = env.new_string("rust/android_support/Activity").unwrap();
-    let support_class: JClass = env
-        .call_method(
-            &cl,
-            "loadClass",
-            "(Ljava/lang/String;)Ljava/lang/Class;",
-            &[JValue::Object(&class_name)],
+    let current_thread = env
+        .call_static_method(
+            "java/lang/Thread",
+            "currentThread",
+            "()Ljava/lang/Thread;",
+            &[],
         )
+        .unwrap()
+        .l()
+        .unwrap();
+    env.call_method(
+        &current_thread,
+        "setContextClassLoader",
+        "(Ljava/lang/ClassLoader;)V",
+        &[(&cl).into()],
+    )
+    .unwrap()
+    .v()
+    .unwrap();
+
+    let cl = env
+        .call_method(
+            &current_thread,
+            "getContextClassLoader",
+            "()Ljava/lang/ClassLoader;",
+            &[],
+        )
+        .unwrap()
+        .l()
+        .unwrap();
+
+    let loader_desc: JString = env
+        .call_method(&cl, "toString", "()Ljava/lang/String;", &[])
         .unwrap()
         .l()
         .unwrap()
         // Force this to be a JClass or an ICE happens:
         // error: internal compiler error: compiler/rustc_infer/src/infer/at.rs:400:21: relating different kinds: jni::objects::JObject<'?197> '?174
         .into();
-    dbg!(&support_class);
+    let loader_desc = env.get_string(&loader_desc).unwrap();
+    dbg!(loader_desc.to_str().unwrap());
+
+    // Find our support class - it happens to be an Activity for now, but we just need one static
+    // method from it
+    // let class_name = env.new_string("rust/android_support/Activity").unwrap();
+    // let support_class: JClass = env
+    //     .call_method(
+    //         &cl,
+    //         "loadClass",
+    //         "(Ljava/lang/String;)Ljava/lang/Class;",
+    //         &[JValue::Object(&class_name)],
+    //     )
+    //     .unwrap()
+    //     .l()
+    //     .unwrap()
+    //     // Force this to be a JClass or an ICE happens:
+    //     // error: internal compiler error: compiler/rustc_infer/src/infer/at.rs:400:21: relating different kinds: jni::objects::JObject<'?197> '?174
+    //     .into();
+    // dbg!(&support_class);
 
     env.call_static_method(
         // Use the class loaded from our Activity' ClassLoader, otherwise JNI won't find the class from the APK
-        &support_class,
+        // &support_class,
+        "rust/android_support/Activity",
         "enableImmersive",
         "(Landroid/app/Activity;)V",
-        &[JValue::Object(&activity)],
+        &[(&activity).into()],
     )
     .unwrap()
     .v()
